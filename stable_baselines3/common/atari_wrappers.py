0933597e2b77c37e23c546c295a98a197ba1bd26
Arman Yekkehkhani <72594459+arman-yekkehkhani@users.noreply.github.com>
2020-11-06 22:04:48 +0330

    fix bug

--- stable_baselines3/common/atari_wrappers.py
+++ stable_baselines3/common/atari_wrappers.py
@@ -196,7 +196,7 @@ class WarpFrame(gym.ObservationWrapper):
         :param frame: environment frame
         :return: the observation
         """
-        if self.region not None:
+        if self.region:
             frame = frame[self.region]
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
